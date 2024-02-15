db08f13420e9cb1ca3974c126a99726f1bbf5c4a
Jasmine71 <qiyinzhang18@gmail.com>
2024-02-14 20:27:00 -0500

    update -> check -> find future time

--- src/exp4a/src/timer.c
+++ src/exp4a/src/timer.c
@@ -19,20 +19,64 @@ void generic_timer_init ( void )
 	gen_timer_init();
 }
 
+int find_mini_sleep(){
+		int global_min = 1000;
+		struct task_struct * q;
+		for(int i = 0; i < NR_TASKS; i++){
+			q = task[i];
+			if(q && q->state == TASK_WAIT && q->sleep_time > 0 && q->sleep_time < global_min){
+				global_min = q->sleep_time;
+			}
+			
+		}
+		return global_min;
+}
+
+void update_future(int new_future){
+	struct task_struct * p;
+	for(int i = 0; i < NR_TASKS; i++){
+		p = task[i];
+		if(p && p->state == TASK_WAIT){
+			p->future_time = new_future;
+		}
+	}
+}
+
+void update_sleep(){
+	struct task_struct * p;
+	for(int i = 0; i < NR_TASKS; i++){
+		p = task[i];
+		if(p && p->state == TASK_WAIT){
+			if(p->future_time <= p->sleep_time){
+				p->sleep_time = p->sleep_time - p->future_time;
+				// printf("updated in %u ticks\n\r", p->sleep_time);
+
+			}
+		}
+	}
+
+}
+
 void handle_generic_timer_irq( void ) 
 {
 	// TODO: In order to implement sleep(t), you should calculate @interval based on t, 
 	// instead of having a fixed @interval which triggers periodic interrupts
-	struct task_strcuts * p;
-	int mini_sleep_time = 1000;
-	int prev_sleep_time = 0;
+	struct task_struct * p;
+
+	update_sleep();
+
+	//check previous sleep() request
 	for(int i = 0; i < NR_TASKS; i++){
 		p = task[i];
-		if(p && p->state == TASK_WAIT && p->sleep_time < mini_sleep_time){
-			mini_sleep_time = p->sleep_time - prev_sleep_time;	
+		if(p && p->state == TASK_WAIT && p->sleep_time == 0){
+			p->state = TASK_RUNNING;
 		}
 	}
-	prev_sleep_time = mini_sleep_time;
-	gen_timer_reset(mini_sleep_time);	
-	printf("Timer interrupt received. next in %u ticks\n\r", interval);
+
+	int next_sleep_time = find_mini_sleep();
+	update_future(next_sleep_time);
+	gen_timer_reset(next_sleep_time * interval);	
+	printf(" Timer interrupt received. next in %u ticks\n\r", next_sleep_time);
 }
+
+
